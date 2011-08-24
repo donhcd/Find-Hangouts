9d32281deeea5084a62412b6f97dd4b4b6eae8b3
Donald Huang <don@factual.com>
2011-08-24 09:45:14 -0700

    add project

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject find-hang-outs "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [com.javadocmd/simplelatlng "RELEASE"]
+                 [funnyplaces "1.0-alpha-SNAPSHOT"]
+                ]
+  :dev-dependencies [[lein-clojars "0.6.0"]
+                     [swank-clojure "1.2.1"]
+                     [lein-run "1.0.0"]]
+)
---
+++ test/find_hang_outs/test/core.clj
@@ -0,0 +1,57 @@
+(ns find-hang-outs.test.core
+  (:use [find-hang-outs.core] :reload)
+  (:use [clojure.test])
+  (:import [com.javadocmd.simplelatlng LatLng]))
+
+;trial code: (map (fn [[c d]] (println d "\n" (map :name c))) (find-hang-outs.core/find-hang-out 6 {:filters {:country "US" :region "CA" :locality "Los Angeles"}} [{:filters {:name {:$bw "pizza"}}} {:filters {:name {:$bw "karaoke"}}} {:q "park"}]))
+
+(deftest add-pois-test
+  (let [lat-lng-1 (LatLng. 10 10.00)
+        lat-lng-2 (LatLng. 10 10)
+        lat-lng-3 (LatLng. 100 100)
+        poi-1     {:LatLng lat-lng-1}
+        poi-2     {:LatLng lat-lng-2}
+        poi-3     {:LatLng lat-lng-3}]
+    (is (= (add-pois 1
+                     [[poi-3 poi-1] 0] ;; a hang-out
+                     [poi-3 poi-2 poi-2] ;; next pois
+                     )
+           [[[{:LatLng lat-lng-3} {:LatLng lat-lng-1} {:LatLng lat-lng-2}] 0]
+            [[{:LatLng lat-lng-3} {:LatLng lat-lng-1} {:LatLng lat-lng-2}] 0]]))
+    (is (= (add-pois 1 [[poi-3] 0] [poi-1])
+           ()))))
+
+(deftest add-pois-to-hang-outs-test
+  (let [lat-lng-1 (LatLng. 10 10.00)
+        lat-lng-2 (LatLng. 10 10)
+        lat-lng-3 (LatLng. 100 100)
+        poi-1 {:LatLng lat-lng-1}
+        poi-2 {:LatLng lat-lng-2}
+        poi-3 {:LatLng lat-lng-3}]
+    (is (= (add-pois-to-hang-outs 1                 ; max dist
+                                 [[[poi-1 poi-1] 0] [[poi-1 poi-2] 1] [[poi-1 poi-3] 0]] ; hang-outs
+                                 [poi-1 poi-2 poi-3] ; next pois
+                                 )
+           [[[poi-1 poi-1 poi-1] 0]
+            [[poi-1 poi-1 poi-2] 0]
+            [[poi-1 poi-3 poi-3] 0]]))))
+
+(deftest get-poi-data-entries-helper-test
+  (is (get-poi-data-entries-helper {:filters {:country "US" :region "CA" :locality "Los Angeles"}}
+                                   {:filters {:name {:$bw "pizza"}}})))
+ 
+(deftest get-poi-data-entries-test
+  (is (get-poi-data-entries {:filters {:country "US" :region "CA" :locality "Los Angeles"}}
+                            [{:filters {:name {:$bw "pizza"}}}
+                             {:filters {:name {:$bw "karaoke"}}}
+                             {:q "park"}])))
+
+(deftest find-hang-out-test
+  (let [hang-outs (find-hang-outs 5  ; max dist
+                                  {:filters {:country "US" :region "CA" :locality "Los Angeles"}} ; main filter
+                                  [{:filters {:name {:$bw "pizza"}}}   ; individual filters
+                                   {:filters {:name {:$bw "karaoke"}}}
+                                   {:q "park"}])]
+    (doseq [poi (map :name (ffirst hang-outs))]
+      (println poi))
+    (println (second (first hang-outs)))))
